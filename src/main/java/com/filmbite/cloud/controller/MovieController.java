90987034477a90579dc99a273d9657b0e32e5c0d
Dhruvan Kadavala <dhruvan.kadavala@dhruvan.kadavala-H97K70DJT2>
2024-09-08 15:04:45 +0530

    Controller created with all movies endPoint

--- src/main/java/com/filmbite/cloud/controller/MovieController.java
+++ src/main/java/com/filmbite/cloud/controller/MovieController.java
@@ -1,4 +1,28 @@
 package com.filmbite.cloud.controller;
 
+import com.filmbite.cloud.data.Movies;
+import com.filmbite.cloud.service.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/filmbite")
 public class MovieController {
+
+    private final MovieService movieService;
+
+    @Autowired
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @GetMapping("/movies")
+    public ResponseEntity<List<Movies>> listAllMovies() {
+        return ResponseEntity.ok(movieService.getAllMovies());
+    }
 }
