075d6ff2bbdb4801bf0b7e1a9845e14dfe025d31
Dhruvan Kadavala <dhruvan.kadavala@dhruvan.kadavala-H97K70DJT2>
2024-09-08 15:03:56 +0530

    repository for Movies created

--- src/main/java/com/filmbite/cloud/repository/MovieRepository.java
+++ src/main/java/com/filmbite/cloud/repository/MovieRepository.java
@@ -1,4 +1,10 @@
 package com.filmbite.cloud.repository;
 
-public class MovieRepository {
+import com.filmbite.cloud.data.Movies;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepository extends MongoRepository<Movies, ObjectId> {
 }
