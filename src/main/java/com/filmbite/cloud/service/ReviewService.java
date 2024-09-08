bf06a55648bd3748650517d6cc76bc864f680ab3
Dhruvan Kadavala <dhruvan.kadavala@dhruvan.kadavala-H97K70DJT2>
2024-09-08 15:24:36 +0530

    Review Services added for creatin review with imdbIds

--- src/main/java/com/filmbite/cloud/service/ReviewService.java
+++ src/main/java/com/filmbite/cloud/service/ReviewService.java
@@ -1,4 +1,32 @@
 package com.filmbite.cloud.service;
 
+import com.filmbite.cloud.data.Movies;
+import com.filmbite.cloud.data.Review;
+import com.filmbite.cloud.repository.ReviewRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ReviewService {
+    private final ReviewRepository reviewRepository;
+    private final MongoTemplate mongoTemplate;
+
+    @Autowired
+    public ReviewService(ReviewRepository reviewRepository, MongoTemplate mongoTemplate) {
+        this.reviewRepository = reviewRepository;
+        this.mongoTemplate = mongoTemplate;
+    }
+
+    public Review createReview(String reviewBody, String imdbId) {
+        Review review = reviewRepository.insert(new Review(reviewBody));
+
+        mongoTemplate.update(Movies.class)
+                .matching(Criteria.where("imdbId").is(imdbId))
+                .apply(new Update().push("reviewIds").value(review)).first();
+
+        return review;
+    }
 }
