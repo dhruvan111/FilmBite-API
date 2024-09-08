500f9d799cebeafeca3d67ea49820d4cb6b39b50
Dhruvan Kadavala <dhruvan.kadavala@dhruvan.kadavala-H97K70DJT2>
2024-09-08 15:04:20 +0530

    Movie service class added

--- src/main/java/com/filmbite/cloud/service/MovieService.java
+++ src/main/java/com/filmbite/cloud/service/MovieService.java
@@ -1,4 +1,22 @@
 package com.filmbite.cloud.service;
 
+import com.filmbite.cloud.data.Movies;
+import com.filmbite.cloud.repository.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class MovieService {
+    private final MovieRepository movieRepository;
+
+    @Autowired
+    public MovieService(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+
+    public List<Movies> getAllMovies() {
+        return movieRepository.findAll();
+    }
 }
